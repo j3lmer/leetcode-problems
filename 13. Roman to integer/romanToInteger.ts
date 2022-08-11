4aa59d1d99080ac69755dbb5ef59e9a31bbbc2c9
j3lmer <jelmer339@gmail.com>
2022-08-11 14:31:55 +0200

    updated romanToInteger

--- 13. Roman to integer/romanToInteger.ts
+++ 13. Roman to integer/romanToInteger.ts
@@ -6,7 +6,7 @@ let answers = [3, 58, 1994];
 
 function init(): string
 {
-	for(let i = 0; i < tests.length; i++){
+	for (let i = 0; i < tests.length; i++) {
 		let ans = romanToInt(tests[i]);
 		if (ans !== answers[i]) {
 			return "Incorrect";
@@ -44,41 +44,46 @@ function romanToInt(s: string): number
 			}
 		})	
 		
-		switch(s[i]){
+		switch (s[i]) {
 			case "I":
-				if(s[i +1] === "V"){
-					curNum = 4;
+				if (s[i +1] === "V") {
+					num += 4;
 					skipNext = true;
+					continue;
 				} 
-				else if(s[i +1] === "X"){
-					curNum = 9;
+				else if (s[i +1] === "X") {
+					num += 9;
 					skipNext = true;
+					continue;
 				} 
 				break;
 
 			case "X":
-				if(s[i +1] === "L"){ 
-					curNum = 40;
+				if (s[i +1] === "L") { 
+					num += 40;
 					skipNext = true;
+					continue;
 				}
-				if(s[i +1] === "C"){
-					curNum = 90;	
+				else if (s[i +1] === "C") {
+					num += 90;	
 					skipNext = true;
+					continue;
 				} 
 				break;
 
 			case "C":
-				if(s[i +1] === "D"){
-					curNum = 400;	
+				if (s[i +1] === "D") {
+					num += 400;	
 					skipNext = true;
+					continue;
 				} 
-				if(s[i +1] === "M"){
-					curNum = 900;	
+				else if (s[i +1] === "M") {
+					num += 900;	
 					skipNext = true;
+					continue;
 				} 
 				break;
 		}
-		
 		num += curNum;
 	}
 	return num;
